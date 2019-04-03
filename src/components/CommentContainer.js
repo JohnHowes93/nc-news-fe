af278f52d877bf4ef9cc3fdf2729e8bb8b895c3d
howes <johnhowes93@googlemail.com>
2019-04-03 19:07:18 +0100

    comment vote re-renders after every vote

--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import { commentDisplayer } from '../utils/CommentContainerUtils';
 import CommentAdder from './CommentAdder';
-const { getCommentsById } = require('../api');
+import CommentDisplayer from './CommentDisplay';
 
 class CommentContainer extends Component {
   state = {
     comments: []
   };
-  componentDidMount() {
-    getCommentsById(this.props.article_id).then(comments => {
-      this.setState({ comments });
-    });
-  }
-
   render() {
     const { comments } = this.state;
     const { user, article_id } = this.props;
     let commentDisplay = <p>no comments to display</p>;
     let addComment = <p>log in to comment</p>;
     if (comments) {
-      commentDisplay = commentDisplayer(comments);
+      commentDisplay = <CommentDisplayer article_id={article_id} />;
     }
     if (user) {
       addComment = <CommentAdder user={user} article_id={article_id} />;
---
+++ src/components/CommentDisplay.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { voteOnComment, getCommentsById } from '../api';
+
+class CommentDisplayer extends Component {
+  state = {
+    comments: [],
+    comment: {}
+  };
+  render() {
+    const { comments } = this.state;
+    console.log(this.state);
+    return comments.map((comment, i) => {
+      return (
+        <div key={comment.comment_id}>
+          <header>
+            <h4>{comment.author}</h4>
+            <h5> {comment.created_at}</h5>
+          </header>
+          <article>{comment.body}</article>
+          <div className="actions">
+            <button
+              type="button"
+              value="1"
+              name={comment.comment_id}
+              onClick={this.handleOnClick}
+            >
+              <span role="img" aria-label="thumbs up">
+                👍
+              </span>
+            </button>
+            <h6>{comment.votes}</h6>
+            <button
+              type="button"
+              value="-1"
+              name={comment.comment_id}
+              onClick={this.handleOnClick}
+            >
+              <span role="img" aria-label="thumbs down">
+                👎
+              </span>
+            </button>
+          </div>
+        </div>
+      );
+    });
+  }
+  componentDidMount() {
+    getCommentsById(this.props.article_id).then(comments => {
+      this.setState({ comments });
+    });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.votes !== this.state.votes) {
+      getCommentsById(this.props.article_id).then(comments => {
+        this.setState({ comments });
+      });
+    }
+  }
+
+  handleOnClick = event => {
+    const postBody = {
+      inc_votes: Number(event.currentTarget.value)
+    };
+    voteOnComment(postBody, event.currentTarget.name).then(votes => {
+      this.setState({ votes });
+    });
+  };
+}
+
+export default CommentDisplayer;
