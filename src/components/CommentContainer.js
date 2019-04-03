8f0f1429b42ec156ca4f05cb1886c73b91d0976c
howes <johnhowes93@googlemail.com>
2019-04-03 15:13:49 +0100

    comments now display conditionally

--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -10,20 +10,25 @@ class CommentContainer extends Component {
       this.setState({ comments });
     });
   }
+
   render() {
     const { comments } = this.state;
-    return comments.map(comment => {
-      return (
-        <div key={comment.comment_id}>
-          <header>
-            <h4>{comment.author}</h4>
-            <h5> {comment.created_at}</h5>
-            <h6>{comment.votes}</h6>
-          </header>
-          <article>{comment.body}</article>
-        </div>
-      );
-    });
+    let commentDisplay = <p>no comments to display</p>;
+    if (comments) {
+      commentDisplay = comments.map(comment => {
+        return (
+          <div key={comment.comment_id}>
+            <header>
+              <h4>{comment.author}</h4>
+              <h5> {comment.created_at}</h5>
+              <h6>{comment.votes}</h6>
+            </header>
+            <article>{comment.body}</article>
+          </div>
+        );
+      });
+    }
+    return <div>{commentDisplay}</div>;
   }
 }
 
