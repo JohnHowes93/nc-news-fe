b3910e1da3bfda8ab908555a2e499fa3278d2864
howes <johnhowes93@googlemail.com>
2019-04-03 20:10:28 +0100

    articles can now be sorted

--- src/api.js
+++ src/api.js
@@ -41,7 +41,6 @@ export const getArrayOfArticles = (params = null) => {
     });
 };
 
-
 export const getArrayOfArticlesByTopic = params => {
   const { topic } = params;
   console.log(topic);
@@ -85,17 +84,6 @@ export const voteOnComment = (postBody, comment_id) => {
     .patch(`${baseUrl}comments/${comment_id}`, postBody)
     .then(returnedComment => {
       return returnedComment.data.patchedComment.votes;
-<<<<<<< HEAD
-=======
-    });
-};
-
-export const voteOnArticle = (postBody, article_id) => {
-  return axios
-    .patch(`${baseUrl}articles/${article_id}`, postBody)
-    .then(returnedArticle => {
-      return returnedArticle.data.article;
->>>>>>> 71348fe40b69b410fae337945d87c826b744367a
     });
 };
 
