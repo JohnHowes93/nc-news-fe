579f5f3a0c0d9f001737bd65df162230992aaf5b
howes <johnhowes93@googlemail.com>
2019-04-10 16:31:24 +0100

    improved error handling

--- src/components/MainArticleContainer.js
+++ src/components/MainArticleContainer.js
@@ -41,64 +41,70 @@ class MainArticleContainer extends Component {
   render() {
     const { article, user } = this.state;
     let deleteArticleButton = <div />;
-    if (article.author === user)
-      deleteArticleButton = (
-        <div>
-          <button
-            type="button"
-            value="1"
-            name={article.article_id}
-            onClick={this.handleDelete}
-          >
-            <span role="img" aria-label="delete">
-              Delete This Article
+    if (article) {
+      if (article.author === user)
+        deleteArticleButton = (
+          <div>
+            <button
+              type="button"
+              value="1"
+              name={article.article_id}
+              onClick={this.handleDelete}
+            >
+              <span role="img" aria-label="delete">
+                Delete This Article
+              </span>
+            </button>
+          </div>
+        );
+    }
+    if (article) {
+      return (
+        <div className="article">
+          <header className="article-header">
+            <span className="article-header-title">{article.title}</span>
+            <span className="article-header-topic">
+              <Link to={`/topics/${article.topic}`}>{article.topic}</Link>
             </span>
-          </button>
+            <span className="article-header-author">
+              <Link to={`/users/${article.author}`}>{article.author}</Link>
+            </span>
+            <span className="article-header-created_at">
+              Posted {moment(article.created_at).fromNow()}
+            </span>
+          </header>
+          <article className="article-body">
+            <p>{article.body}</p>
+          </article>
+          <div className="actions">
+            {article.votes}{' '}
+            <button
+              type="button"
+              value="1"
+              name={article.article_id}
+              onClick={this.handleOnClick}
+            >
+              <span role="img" aria-label="thumbs up">
+                👍
+              </span>
+            </button>
+            <button
+              type="button"
+              value="-1"
+              name={article.article_id}
+              onClick={this.handleOnClick}
+            >
+              <span role="img" aria-label="thumbs down">
+                👎
+              </span>
+            </button>
+            {deleteArticleButton}
+          </div>
         </div>
       );
-    return (
-      <div className="article">
-        <header className="article-header">
-          <span className="article-header-title">{article.title}</span>
-          <span className="article-header-topic">
-            <Link to={`/topics/${article.topic}`}>{article.topic}</Link>
-          </span>
-          <span className="article-header-author">
-            <Link to={`/users/${article.author}`}>{article.author}</Link>
-          </span>
-          <span className="article-header-created_at">
-            Posted {moment(article.created_at).fromNow()}
-          </span>
-        </header>
-        <article className="article-body">
-          <p>{article.body}</p>
-        </article>
-        <div className="actions">
-          {article.votes}{' '}
-          <button
-            type="button"
-            value="1"
-            name={article.article_id}
-            onClick={this.handleOnClick}
-          >
-            <span role="img" aria-label="thumbs up">
-              👍
-            </span>
-          </button>
-          <button
-            type="button"
-            value="-1"
-            name={article.article_id}
-            onClick={this.handleOnClick}
-          >
-            <span role="img" aria-label="thumbs down">
-              👎
-            </span>
-          </button>
-          {deleteArticleButton}
-        </div>
-      </div>
-    );
+    } else {
+      return <div />;
+    }
   }
 }
 
